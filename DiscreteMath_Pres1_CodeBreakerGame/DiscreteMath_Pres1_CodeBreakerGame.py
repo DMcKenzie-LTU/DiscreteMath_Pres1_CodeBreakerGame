4974695aa7cc24e0cf7394d64a7932efbe0549d5
dmckenzie <dmckenzie@ltu.edu>
2026-02-01 21:48:53 -0500

    Python Game Set up

--- DiscreteMath_Pres1_CodeBreakerGame/DiscreteMath_Pres1_CodeBreakerGame.py
+++ DiscreteMath_Pres1_CodeBreakerGame/DiscreteMath_Pres1_CodeBreakerGame.py
@@ -1,1 +1,24 @@
+﻿import pygame # Import the Pygame library
 
+# Initialization 
+pygame.init() # Initialize Pygame
+
+# Display setup
+screen = pygame.display.set_mode((800, 600)) # Set the window size
+pygame.display.set_caption("Code Breaker Game") # Set the window title
+font = pygame.font.Font(None, 48) # Set the font and size
+
+# Parameters
+running = True # Main loop flag
+
+# Main loop
+while running: # Main loop
+    for event in pygame.event.get():    # Event handling
+        if event.type == pygame.QUIT:   # Quit event
+            running = False             # Exit the main loop
+            
+    screen.fill((240,240,240))  # Fill the screen with a light gray color   
+    pygame.display.flip()       # Update the display
+ 
+# Close Game    
+pygame.quit()   # Quit Pygame
